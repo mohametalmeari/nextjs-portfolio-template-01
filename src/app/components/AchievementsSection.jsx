c5abed1160c3932f0e5376ed348fef4b2e5ed5e4
mohametalmeari <mohamet.almeari@gmail.com>
2024-04-21 03:51:52 +0300

    Implement animated numbers in AchievementsSection

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import dynamic from "next/dynamic";
+
+const AnimatedNumbers = dynamic(
+  () => {
+    return import("react-animated-numbers");
+  },
+  { ssr: false }
+);
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100+",
+    value: "100",
+    postfix: "+",
   },
   {
+    prefix: "~",
     metric: "Users",
-    value: "1M+",
+    value: "100,000",
   },
   {
     metric: "Awards",
@@ -15,7 +26,7 @@ const achievementsList = [
   },
   {
     metric: "Years",
-    value: "5+",
+    value: "5",
   },
 ];
 
@@ -28,8 +39,22 @@ const AchievementsSection = () => {
             key={index}
             className="flex flex-col items-center justify-center mx-4"
           >
-            <h2 className="text-white text-4xl font-bold">
-              {achievement.value}
+            <h2 className="text-white text-4xl font-bold flex flex-row">
+              {achievement.prefix}
+              <AnimatedNumbers
+                includeComma
+                animateToNumber={parseInt(achievement.value)}
+                locale="en-US"
+                className="text-white text-4xl font-bold"
+                configs={(_, index) => {
+                  return {
+                    mass: 1,
+                    friction: 10,
+                    tensions: 140 * (index + 1),
+                  };
+                }}
+              />
+              {achievement.postfix}
             </h2>
             <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
           </div>
